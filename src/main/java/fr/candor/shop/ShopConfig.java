f2c614332b4c2d4de41be3c66be6b07d9e5992ed
Ibramsou <kasobanboui@gmail.com>
2023-07-18 20:18:22 +0200

    plugin gradle setup

---
+++ src/main/java/fr/candor/shop/ShopConfig.java
@@ -0,0 +1,4 @@
+package fr.candor.shop;
+
+public class ShopConfig {
+}
---
+++ src/main/java/fr/candor/shop/ShopPlugin.java
@@ -0,0 +1,11 @@
+package fr.candor.shop;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ShopPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        this.getLogger().info("Enabled plugin for Candor");
+    }
+}
